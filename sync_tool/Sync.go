5244aa57b909a6c42ebecd9cf78cc78a7f0ba954
Jacky <jacky.li@joy-games.online>
2022-03-08 17:36:40 +0800

    feat:map sync tool
    (cherry picked from commit 69f1b55905c1e8ea59a2999ab32382d081a84d90)

---
+++ sync_tool/Sync.go
@@ -0,0 +1,41 @@
+package sync_tool
+
+import "sync"
+
+type MapLocker struct {
+	sMap sync.Map
+}
+
+var rwMutexPool = sync.Pool{New: func() interface{} {
+	return &sync.RWMutex{}
+}}
+
+func (m *MapLocker) Get(key interface{}) ( lk *sync.RWMutex,loaded bool) {
+	lk = rwMutexPool.Get().(*sync.RWMutex)
+	l, loaded := m.sMap.LoadOrStore(key, lk)
+	if loaded {
+		rwMutexPool.Put(lk)
+		lk = l.(*sync.RWMutex)
+	}
+	return
+}
+
+func (m *MapLocker) Free(key interface{})(loaded bool) {
+	l , loaded := m.sMap.LoadAndDelete(key)
+	if loaded{
+		rwMutexPool.Put(l)
+	}
+	return false
+}
+
+
+
+var maplocker = MapLocker{}
+
+func Get(key interface{}) (lk *sync.RWMutex, exist bool) {
+	return maplocker.Get(key)
+}
+
+func Free(key interface{}) ( exist bool) {
+	return  maplocker.Free(key)
+}
