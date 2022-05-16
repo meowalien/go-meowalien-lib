3ecff6dd68b4d99d3ac5cbb487ebfbe8ebcf24ba
Jacky <jacky.li@joy-games.online>
2022-05-16 10:39:38 +0800

    feat:aes tool

--- sign/Sign.go
+++ sign/Sign.go
@@ -8,11 +8,10 @@ import (
 
 func Md5Sign(bt []byte) string {
 	md5Ctx := md5.New()
-	_ , err := md5Ctx.Write(bt)
-	if err != nil{
-		log.Println("error when Write to md5 Hash: ",err.Error())
+	_, err := md5Ctx.Write(bt)
+	if err != nil {
+		log.Println("error when Write to md5 Hash: ", err.Error())
 		return ""
 	}
 	return hex.EncodeToString(md5Ctx.Sum(nil))
 }
-
