597892ed32336c3d31747cc3c27cf59e82a14856
jacky_li <jacky_li@paradise-soft.com.tw>
2022-08-29 15:00:19 +0800

    remove debug waitLimiter output

--- synchronize_limiter/WaitLimiter.go
+++ synchronize_limiter/WaitLimiter.go
@@ -3,7 +3,6 @@ package synchronize_limiter
 import (
 	"context"
 	"errors"
-	"fmt"
 	"github.com/meowalien/go-meowalien-lib/errs"
 	"sync"
 )
@@ -19,13 +18,11 @@ type waitLimiter struct {
 
 func (s *waitLimiter) Stop(ctx context.Context) {
 	s.cancel()
-	fmt.Println("before wait")
 	for len(s.waitingTaskQueue) != 0 {
-
+		//	wait Consumer to consume all task
 	}
 	close(s.stopChan)
 	s.wait.Wait() // wait for queue to be empty
-	fmt.Println("after wait")
 
 }
 
@@ -48,7 +45,6 @@ func (s *waitLimiter) Do(ctx context.Context, f func()) (err error) {
 func (s *waitLimiter) startConsumer() {
 	s.wait.Add(s.threadCount)
 	for i := 0; i < s.threadCount; i++ {
-		fmt.Println("start thread: ", i)
 		go func(i int) {
 			defer s.wait.Done()
 			for {
@@ -56,13 +52,10 @@ func (s *waitLimiter) startConsumer() {
 				case <-s.stopChan:
 					return
 				case f := <-s.waitingTaskQueue:
-					fmt.Println("run Thead: ", len(s.waitingTaskQueue))
 					f()
 					continue
 				}
 			}
-
-			fmt.Println("close thread: ", i)
 		}(i)
 	}
 }
