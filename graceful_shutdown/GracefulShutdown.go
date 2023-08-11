f226f16c2f42ef02f833f2da11f4428075a44bc2
jacky_li <jacky_li@Jackys-MacBook-Pro.local>
2023-08-11 09:58:18 +0800

    edit graceful_shutdown

--- graceful_shutdown/GracefulShutdown.go
+++ graceful_shutdown/GracefulShutdown.go
@@ -1,38 +1,89 @@
 package graceful_shutdown
 
 import (
-	"fmt"
-	"github.com/meowalien/go-meowalien-lib/contexts"
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
-type GracefulShutdownLevel interface {
-	PromiseDone() (chFc <-chan func())
-	Close()
-	fmt.Stringer
-	NextLevel(name string) *gracefulShutdownLevel
+var afterAllStop = make(chan struct{})
+
+var c = make(chan os.Signal, 1)
+
+func init() {
+	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-c
+		signal.Stop(c)
+		runStopStack()
+	}()
 }
 
-func NewRootLevel(name string) *gracefulShutdownLevel {
-	return &gracefulShutdownLevel{ContextGroup: contexts.NewContextGroup(nil), name: name}
-}
-
-type gracefulShutdownLevel struct {
-	contexts.ContextGroup
-	name  string
-	level uint
-}
-
-func (g *gracefulShutdownLevel) String() string {
-	if g.name == "" {
-		return fmt.Sprintf("Level%d", g.level)
+func StopNow(ctx context.Context) error {
+	select {
+	case <-ctx.Done():
+		// abort
+		return ctx.Err()
+	case <-afterAllStop:
+		// already stopped
+		return nil
+	case c <- syscall.SIGTERM:
+		// successfully sent
+		select {
+		case <-ctx.Done():
+			// abort
+			return ctx.Err()
+		case <-afterAllStop:
+			// successfully stopped
+			return nil
+		}
+	default:
+		// already notified, wait for stop
+		select {
+		case <-ctx.Done():
+			// abort
+			return ctx.Err()
+		case <-afterAllStop:
+			// successfully stopped
+			return nil
+		}
 	}
-	return g.name
 }
 
-func (g *gracefulShutdownLevel) Level() uint {
-	return g.level
+type stopMission struct {
+	f    func()
+	name string
 }
 
-func (g *gracefulShutdownLevel) NextLevel(name string) *gracefulShutdownLevel {
-	return &gracefulShutdownLevel{ContextGroup: g.ChildGroup(), name: name, level: g.level + 1}
+var stopStack = make([]stopMission, 0)
+
+func Add(name string, f func()) {
+	stopStack = append(stopStack, stopMission{
+		f:    f,
+		name: name,
+	})
+}
+
+func runStopStack() {
+	for i := len(stopStack) - 1; i >= 0; i-- {
+		func() {
+			defer func() {
+				if r := recover(); r != nil {
+					log.Printf("Panic when running stopStack:%s , %+v\n", stopStack[i].name, r)
+				}
+			}()
+			stopStack[i].f()
+		}()
+	}
+}
+
+func WaitAllStop(ctx context.Context) error {
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case <-afterAllStop:
+		return nil
+	}
 }
